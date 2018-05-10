9ef0b6ceb5a7432aa4c34ba0da715795badd727e
Pedro <petermoca2@gmail.com>
2018-05-10 01:46:54 -0500

    Sign Up App added

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView #TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
-# Create your views here.
 
+
+# Create your views here.
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
@@ -11,3 +14,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__' #This loads all the fields declared in the model
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = [
+        'title',
+        'body'
+    ]
+    template_name = 'post_edit.html'
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
--- the_bproject/settings.py
+++ the_bproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
